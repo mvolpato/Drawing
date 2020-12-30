e14858fb064d4838b045f8105eee3d040c01c225
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-30 16:54:49 +0100

    Day 44

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,57 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+        
+        // now send the main path back
+        return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
     var body: some View {
-        Triangle()
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-            .frame(width: 300, height: 300)
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-            .stroke(Color.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
-        Circle()
-            .stroke(Color.blue, lineWidth: 40)
-        Circle()
-            .strokeBorder(Color.blue, lineWidth: 40)
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount: CGFloat = 0
-
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-
-        return path
-    }
-}
-
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
+struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+                .scaleEffect(0.5)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+            
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("cupcakes"), scale: 0.1), lineWidth: 20)
+                .frame(width: 300, height: 100)
+            
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 100, height: 100)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
